384cea2c7f30ea2ebe9d34f7381a7bec61097a6a
Ashvary1996 <ashvarygidian1996@gmail.com>
2026-03-09 22:02:01 +0530

    test rounde added

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.json("Version 1 running");
 });
+app.get("/test", (req, res) => {
+  res.json("test route is working.");
+});
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
